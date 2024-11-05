467830e9a6ca01db6c9e5a006c12a33e4ebe26b0
Munene <tmunene3@gmail.com>
2024-11-04 22:23:33 -0500

    sort of working -- 1 test

--- mcts.py
+++ mcts.py
@@ -21,8 +21,15 @@ class Node:
     def state(self) -> State:
         return self._state
     
+    @property
+    def visits(self) -> int:
+        return self._visits
+
+    def number_of_children(self) -> int:
+        return len(self._edges)
+    
     def add_edge(self, action, child) -> None:
-        self._edges.append(Edge(action, child))
+        self._edges.append(Edge(action, child, self))
         return
 
     def update_value(self, reward: float) -> None:
@@ -33,28 +40,38 @@ class Node:
         self._visits += 1
         return 
     
-    def expand(self) -> None:
-        if not self._edges:
-            possible_actions = self._state.get_actions()
-            children_states = [Node(self._state.successor(action)) for action in possible_actions]
-            list_of_edges = [self.add_edge(action, child) for action, child in zip(possible_actions, children_states)]
-            self._edges = list_of_edges
+    def add_children(self) -> None:
+        possible_actions = self._state.get_actions()
+        children_states = [Node(self._state.successor(action)) for action in possible_actions]
+        for action, child in zip(possible_actions, children_states):
+            self.add_edge(action, child)
         return
     
+    def expand(self) -> 'Node':
+        """
+            A node is expandable if it is non-terminal and has been visited
+            If a node is expandable: return a random child
+            else: return same node
+        """
+        if not self._state.is_terminal() and self.visits > 0:
+            self.add_children()
+            return choice(self._edges).child
+        return self
+    
     def next_child_to_explore(self) -> 'Edge':
-        def ucb1(edge: 'Edge') -> float:
+        def ucb(edge: 'Edge') -> float:
             if edge._visits == 0:
                 return float('inf')
-            return edge.child._value + sqrt(2 * log(self._visits) / edge._visits)
-        return max(self._edges, key=ucb1)
+            average_child_value = edge.child.value / edge.child.number_of_children() if edge.child.number_of_children() else 0
+            return average_child_value + sqrt(2 * log(self.visits) / edge.visits)
+        return max(self._edges, key=ucb)
     
     def simulate(self) -> float:
         state = self._state
-        possible_actions = state.get_actions()
-        while possible_actions:
-            action = choice(possible_actions)
-            state = state.successor(action)
+        while not state.is_terminal():
             possible_actions = state.get_actions()
+            random_action = choice(possible_actions)
+            state = state.successor(random_action)
         return state.payoff()
 
     def is_leaf_node(self) -> bool:
@@ -72,12 +89,16 @@ class Node:
 class RootNode(Node):
     def __init__(self, state: State):
         super().__init__(state)
+        self.add_children()
 
     @property
     def edges(self) -> List['Edge']:
         return self._edges
     
     def traverse(self) -> Tuple[Deque['Edge'], Node]:
+        """
+            Use UCB formula to guide tree traversal from root to leaf node
+        """
         node = self
         edges_to_leaf_node = deque()
         while not node.is_leaf_node():
@@ -107,23 +128,27 @@ class Edge:
     @property
     def parent(self) -> Node:
         return self._parent
+    
+    @property
+    def visits(self) -> int:
+        return self._visits
 
 
 def monte_carlo_tree_search(position: State, duration: float):
     start_time = time()
     root = RootNode(position)
     
-    while time() - start_time < duration:
+    while (time() - start_time) < duration:
         # Traverse: Choode path from root to best leaf node
         edges_to_leaf_node, best_child = root.traverse()
         # Expand: Add children to best leaf node if possible
-        best_child.expand()
+        node_to_simulate_play = best_child.expand()
         # Simulate: Simulate a random game from best leaf node to terminal state
-        reward = best_child.simulate()
+        reward = node_to_simulate_play.simulate()
         # Backpropagate: Update value of nodes in path from root to best leaf node
         best_child.backpropagate(reward, edges_to_leaf_node)
     
-    return max(root.edges, key=lambda edge: edge.child.value).action
+    return max(root.edges, key=lambda edge: edge.child.visits).action
 
 def mcts_policy(duration: float):
     return lambda position: monte_carlo_tree_search(position, duration)
