2b9b8232190fba98026e9eb9283bdd8b7d447e64
Munene <tmunene3@gmail.com>
2024-11-05 23:26:44 -0500

    Upgraded to uct2

--- mcts.py
+++ mcts.py
@@ -7,8 +7,9 @@ from random import choice
 from collections import deque
 
 class Node:
-    def __init__(self, state: State):
+    def __init__(self, state: State, root: 'RootNode'):
         self._state = state
+        self._root = root
         self._edges = [] 
         self._value = 0
         self._visits = 0
@@ -22,13 +23,21 @@ class Node:
         return self._state
     
     @property
+    def root(self) -> 'RootNode':
+        return self._root
+    
+    @property
+    def edges(self) -> List['Edge']:
+        return self._edges
+    
+    @property
     def visits(self) -> int:
         return self._visits
 
     def number_of_children(self) -> int:
         return len(self._edges)
     
-    def add_edge(self, action, child) -> None:
+    def add_edge(self, action, child: 'Node') -> None:
         self._edges.append(Edge(action, child, self))
         return
 
@@ -40,21 +49,26 @@ class Node:
         self._visits += 1
         return 
     
-    def add_children(self) -> None:
-        possible_actions = self._state.get_actions()
-        children_states = [Node(self._state.successor(action)) for action in possible_actions]
-        for action, child in zip(possible_actions, children_states):
+    def add_children(self, root: 'RootNode') -> None:
+        possible_actions = self.state.get_actions()
+        successor_states = [self.state.successor(action) for action in possible_actions]
+        for successor_state, action in zip(successor_states, possible_actions):
+            if successor_state not in root._Tree:
+                child = Node(successor_state, root)
+                root.update_node_in_tree(successor_state, child)
+            else:
+                child = root.get_node_from_tree(successor_state)
             self.add_edge(action, child)
         return
     
-    def expand(self) -> 'Node':
+    def expand(self, root: 'RootNode') -> 'Node':
         """
-            A node is expandable if it is non-terminal and has been visited
+            A node is expandable if it is non-terminal and has been visited. Confirmed that node is non-terminal in main loop.
             If a node is expandable: return a random child
             else: return same node
         """
-        if not self._state.is_terminal() and self.visits > 0:
-            self.add_children()
+        if self.visits > 0:
+            self.add_children(root)
             return choice(self._edges).child
         return self
     
@@ -63,7 +77,8 @@ class Node:
             if edge._visits == 0:
                 return float('inf')
             average_child_value = edge.child.value / edge.child.number_of_children() if edge.child.number_of_children() else 0
-            return average_child_value + sqrt(2 * log(self.visits) / edge.visits)
+            t = sum(edge.visits for edge in self.edges)
+            return average_child_value + sqrt(2 * log(t) / edge.visits)
         return max(self._edges, key=ucb)
     
     def simulate(self) -> float:
@@ -78,6 +93,8 @@ class Node:
         return not self._edges
     
     def backpropagate(self, reward: float, edges_to_root: Deque['Edge']) -> None:
+        # Update visit count for terminal node
+        self.update_visits()
         while edges_to_root:
             edge = edges_to_root.popleft()
             edge.update_visits()
@@ -88,8 +105,10 @@ class Node:
 
 class RootNode(Node):
     def __init__(self, state: State):
-        super().__init__(state)
-        self.add_children()
+        super().__init__(state, self)
+        self._Tree = dict() # state -> node
+        self._Tree[state] = self
+        self.add_children(self)
 
     @property
     def edges(self) -> List['Edge']:
@@ -106,6 +125,16 @@ class RootNode(Node):
             edges_to_leaf_node.appendleft(edge)
             node = edge.child
         return edges_to_leaf_node, node
+    
+    def update_node_in_tree(self, state: State, node: Node) -> None:
+        """
+            Update the tree with the new node
+        """
+        self._Tree[state] = node
+        return
+    
+    def get_node_from_tree(self, state: State) -> Node:
+        return self._Tree[state]
     
 class Edge:
     def __init__(self, action: Card | List[int], child: Node, parent: Node):
@@ -139,16 +168,19 @@ def monte_carlo_tree_search(position: State, duration: float):
     root = RootNode(position)
     
     while (time() - start_time) < duration:
-        # Traverse: Choode path from root to best leaf node
-        edges_to_leaf_node, best_child = root.traverse()
+        # Traverse: Choose path from root to best leaf node
+        edges_to_leaf_node, node_to_simulate_play = root.traverse()
         # Expand: Add children to best leaf node if possible
-        node_to_simulate_play = best_child.expand()
-        # Simulate: Simulate a random game from best leaf node to terminal state
-        reward = node_to_simulate_play.simulate()
+        if not node_to_simulate_play.state.is_terminal():
+            node_to_simulate_play = node_to_simulate_play.expand(root)
+            # Simulate: Simulate a random game from best leaf node to terminal state
+            reward = node_to_simulate_play.simulate()
+        else:
+            reward = node_to_simulate_play.state.payoff()
         # Backpropagate: Update value of nodes in path from root to best leaf node
-        best_child.backpropagate(reward, edges_to_leaf_node)
+        node_to_simulate_play.backpropagate(reward, edges_to_leaf_node)
     
-    return max(root.edges, key=lambda edge: edge.child.visits).action
+    return max(root.edges, key=lambda edge: edge.child.value / edge.child.visits).action
 
 def mcts_policy(duration: float):
     return lambda position: monte_carlo_tree_search(position, duration)
